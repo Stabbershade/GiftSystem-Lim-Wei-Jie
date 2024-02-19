abc52c3aee675e0abc38c895bc5e79f35497f77e
Stabb <Stabbershade@hotmail.com>
2024-02-20 03:11:49 +0800

    feat: Able to load data from .csv

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import * as fs from 'fs';
 import { parse } from 'csv-parse';
+import { error } from 'console';
 
 type StaffToPass = {
     staff_id : string,
@@ -11,4 +12,30 @@ type Redemption = {
     team_name: string,
     redeemed_at: number
 }
+
+class GiftSystem {
+    private staffToPassData : StaffToPass[] = []
+    private redemptionData : Redemption[] = []
+
+    constructor(file:string){
+        this.loadDataFromCSV(file)
+    }
+
+    private loadDataFromCSV(file_path: string): void {
+        const fileContent = fs.readFileSync(file_path, { encoding: 'utf-8' })
+        parse(fileContent , {
+            delimiter: ",",
+            columns : true,
+        }, (error, result: StaffToPass) => {
+            if (error) {
+              console.error(error);
+            }
+            this.staffToPassData.push(result)
+        }).on('end' , () =>{
+            console.log("Data Successfully Loaded")
+        })
+    }
+}
+
+const system = new GiftSystem('./sample/staff-id-to-team-mapping.csv');
 
