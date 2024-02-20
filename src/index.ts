8862f027d37c203cf862d3cd8edc05294b14e582
Stabb <Stabbershade@hotmail.com>
2024-02-20 13:44:02 +0800

    feat: Userinput and runnable implementation and excluding data set in gitignore

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,42 @@
-import * as fs from 'fs';
-import { parse } from 'csv-parse';
-import { error } from 'console';
+import GiftSystem from "./Giftsystem";
 
-type StaffToPass = {
-    staff_id : string,
-    team_name: string,
-    created_at: number
+const argv = process.argv;
+
+const readline = require('readline');
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const GiftsystemInstance = new GiftSystem();
+GiftsystemInstance.loadDataFromCSV("./sample/" + argv[2]).then(() => {
+    searchPrompt()
+})
+
+function searchPrompt() {
+    rl.question('GiftRedemption> ', (inputsteam: String) => {
+        const input: string[] = inputsteam.split(" ")
+        switch (input[0].toLowerCase()) {
+            case "lookup":
+                GiftsystemInstance.lookUpStaffToTeam(input[1])
+                break;
+            case "redeem":
+                GiftsystemInstance.addNewRedemption(input[1])
+                break;
+            case "list":
+                GiftsystemInstance.listRedeemed()
+                break;
+            case "left":
+                GiftsystemInstance.RemainingTeamsToRedeem()
+                break;
+            case "exit":
+                rl.close()
+                return
+            default:
+                console.log("Invalid Command")
+                break;
+        }
+        searchPrompt();
+    });
 }
-
-type Redemption = { 
-    team_name: string,
-    redeemed_at: number
-}
-
-class GiftSystem {
-    private staffToPassData : StaffToPass[] = []
-    private redemptionData : Redemption[] = []
-
-    constructor(file:string){
-        this.loadDataFromCSV(file)
-    }
-
-    private loadDataFromCSV(file_path: string): void {
-        const fileContent = fs.readFileSync(file_path, { encoding: 'utf-8' })
-        parse(fileContent , {
-            delimiter: ",",
-            columns : true,
-        }, (error, result: StaffToPass) => {
-            if (error) {
-              console.error(error);
-            }
-            this.staffToPassData.push(result)
-        }).on('end' , () =>{
-            console.log("Data Successfully Loaded")
-        })
-    }
-}
-
-const system = new GiftSystem('./sample/staff-id-to-team-mapping.csv');
 
