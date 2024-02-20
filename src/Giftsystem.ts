2699f5e95203c32d747ff11faec09b9c21b6b55f
Stabb <Stabbershade@hotmail.com>
2024-02-20 13:41:07 +0800

    feat: add lookUpStaffToTeam, verifyRedemption, addNewRedemption, listRedeemed, RemainingTeamsToRedeem and fixing loadDataFromCSV

---
+++ src/Giftsystem.ts
@@ -0,0 +1,106 @@
+import * as fs from 'fs';
+import { parse } from 'csv-parse';
+
+type StaffToPass = {
+    staff_pass_id: string,
+    team_name: string,
+    created_at: number
+}
+
+type Redemption = {
+    team_name: string,
+    redeemed_at: number
+}
+
+class GiftSystem {
+    private staffToPassData: StaffToPass[] = []
+    private redemptionData: Redemption[] = []
+
+    loadDataFromCSV(file_path: string): Promise<void> {
+        return new Promise((resolve, reject) => {
+            const header = ["staff_pass_id", "team_name", "created_at"]
+            const fileContent = fs.readFileSync(file_path, { encoding: 'utf-8' })
+            parse(fileContent, {
+                delimiter: ",",
+                columns: header,
+                from_line: 2
+            }, (error, result: StaffToPass[]) => {
+                if (error) {
+                    console.error(error);
+                }
+                result.forEach((value: StaffToPass) => {
+                    this.staffToPassData.push(value)
+                })
+            }).on('end', () => {
+                console.log("Data Successfully Loaded")
+                resolve()
+            }).on('error', (err) => {
+                console.log("Data unable to initilise")
+                reject(err)
+            })
+        })
+    }
+
+    lookUpStaffToTeam(staff_id: string): void {
+
+        const lookup = this.staffToPassData.find((staff) => {
+            return staff.staff_pass_id === staff_id
+        })
+        if (lookup) {
+            console.log(`${staff_id} is from team ${lookup.team_name}`)
+        }
+        else {
+            console.log(`${staff_id} do not exist`)
+        }
+
+    }
+
+    verifyRedemption(team_name: string): boolean {
+        const lookup = this.redemptionData.find((redeemed) => {
+            return redeemed.team_name === team_name
+        })
+
+        return !lookup
+    }
+
+    addNewRedemption(team_name: string): void {
+        if (this.verifyRedemption(team_name)) {
+            this.redemptionData.push({
+                team_name: team_name,
+                redeemed_at: parseInt(String(Date.now()))
+            })
+            console.log(`Team: ${team_name} successfully redeemed`)
+        }
+        else {
+            console.log(`Team: ${team_name} has already redeemed`)
+        }
+    }
+
+    listRedeemed(): void {
+        this.redemptionData.forEach((data) => {
+            console.log(`${data.team_name} has redeemed gift on ${new Date(parseInt(String(data.redeemed_at)))}`)
+        })
+    }
+
+    RemainingTeamsToRedeem(): void {
+        let teamSet = new Set<string>()
+
+        this.staffToPassData.forEach((data) => {
+            teamSet.add(data.team_name)
+        })
+
+        this.redemptionData.forEach((data) => {
+            if (teamSet.has(data.team_name)) {
+                teamSet.delete(data.team_name)
+            }
+        })
+
+        console.log(`Teams that didn't redeem yet: ${Array.from(teamSet)}`)
+    }
+
+
+}
+
+export default GiftSystem;
+
+
